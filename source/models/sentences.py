ee206be104017f5ccbec85473fb2688614bf2e76
Antonio Menta <mentared@gmail.com>
2023-09-27 23:54:51 +0200

    Modified notebook to insert the sentences.

--- source/models/sentences.py
+++ source/models/sentences.py
@@ -5,9 +5,9 @@ class SentenceModel(db.Model):
     __tablename__ = "sentences"
 
     sentence_id = db.Column(db.Integer, primary_key=True)
-    sentence_original = db.Column(db.String(500), unique=True, nullable=False)
-    sentence_simplified = db.Column(db.String(500), unique=True, nullable=False)
-    document_name = db.Column(db.String(100), unique=True, nullable=False)
+    sentence_original = db.Column(db.String(500), unique=False, nullable=False)
+    sentence_simplified = db.Column(db.String(500), unique=False, nullable=False)
+    document_name = db.Column(db.String(100), unique=False, nullable=False)
     document_sentence_id = db.Column(db.Integer, nullable=True)
 
     def __init__(self, sentence_original,
