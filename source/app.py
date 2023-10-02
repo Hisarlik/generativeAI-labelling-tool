159a9cc6fecf5ae34ae3402b251e45d94c17dd22
Antonio Menta <mentared@gmail.com>
2023-10-02 23:32:18 +0200

    database added.

--- source/app.py
+++ source/app.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database_new.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SECRET_KEY'] = secret_key
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -85,58 +86,3 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-def create_app():
-    # creates an application instance app using Flask() class
-    app = Flask(__name__)
-    secret_key = secrets.token_hex(16)
-    app.config['SECRET_KEY'] = secret_key
-    # swagger documentation details
-    app.config["PROPAGATE_EXCEPTIONS"] = True
-    app.config["API_TITLE"] = "Employee Crud Example"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config[
-        "OPENAPI_SWAGGER_UI_URL"
-    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-
-    basedir = os.path.abspath(os.path.dirname(__file__))
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'sqlite:///' + os.path.join(basedir, 'database.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Connect the database to the app instance
-    db.init_app(app)
-
-    api = Api(app)
-
-    login_manager.init_app(app)
-
-
-
-
-
-
-
-    with app.app_context():
-        db.create_all()
-
-    #register employee blueprint
-    api.register_blueprint(SentenceBlueprint)
-
-    return app
