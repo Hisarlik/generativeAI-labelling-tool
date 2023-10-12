7f44d6b6b5c8d4c995c271389952d13942069347
Antonio Menta <mentared@gmail.com>
2023-10-12 18:12:12 +0200

    gunicorn included. look and feel changed.

--- source/app.py
+++ source/app.py
@@ -7,8 +7,6 @@ import os
 from source.db import db
 from source.models.user import User, LoginForm, RegisterationForm
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-
 from source.resources.sentence import sentblueprint as sentenceBlueprint
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
@@ -79,18 +77,13 @@ def signup():
     form = RegisterationForm()
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method= 'sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-
-        return '<h1> New user has been created </h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        form = LoginForm()
+        return redirect('login')
     return render_template('signup.html', form=form)
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html', name= current_user.username)
 @app.route('/logout')
 @login_required
 def logout():
--- source/models/user.py
+++ source/models/user.py
@@ -6,7 +6,6 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key= True)
     username = db.Column(db.String(15), unique= True)
-    email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
 class LoginForm(FlaskForm):
@@ -14,6 +13,5 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=10)])
     remember = BooleanField('Remember me')
 class RegisterationForm(FlaskForm):
-    email= StringField('Email', validators=[InputRequired(),Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=15)])
